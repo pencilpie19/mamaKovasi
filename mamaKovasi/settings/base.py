a76feba2fc4818cc9f94f4500f48a40b46475647
Muhammet Bulut <17701029@mersin.edu.tr>
2020-11-25 21:24:53 +0300

    ikinci deneme

--- mamaKovasi/settings/base.py
+++ mamaKovasi/settings/base.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'ydnbkd(+jv^v7$d%j3#+%r-q!rl1cz$361$jiz22-5ro_f)vdl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["127.0.0.1","68.183.66.88","mamakovasi.com","www.mamakovasi.com"]
+ALLOWED_HOSTS = ["127.0.0.1","134.209.80.44","mamakovasi.com","www.mamakovasi.com"]
 
 
 # Application definition
